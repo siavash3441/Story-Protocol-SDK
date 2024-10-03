ea58f4063eae224ffd69fc98f4efd827dda9ac72
Siavash Seddighi <130931966+siavash3441@users.noreply.github.com>
2024-10-03 12:20:19 +0330

    Create install2.sh

---
+++ install2.sh
@@ -0,0 +1,26 @@
+show "Installing npm dependencies..."
+npm install
+echo
+
+read -p "Enter your wallet private key: " WALLET
+read -p "Enter Pinata JWT token: " JWT
+
+
+cat <<EOF > .env
+WALLET_PRIVATE_KEY=$WALLET
+PINATA_JWT=$JWT
+EOF
+
+
+show "Running npm script to create SPG collection..."
+npm run create-spg-collection
+echo
+
+read -p "Enter NFT contract address: " CONTRACT
+echo
+
+echo "NFT_CONTRACT_ADDRESS=$CONTRACT" >> .env
+
+show "Running npm script for metadata..."
+npm run metadata
+echo
